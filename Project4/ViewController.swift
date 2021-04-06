95d255ee50de5232be55e2dd9ed7d683f998ab9d
Edson <edson_neto1996@hotmail.com>
2021-04-06 11:23:48 -0300

    Adicionando botão de refresh e barra de progresso

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -10,6 +10,7 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate { //Nosso ViewCOntroller extende UIViewController mas ele conforma-se aos protocolos do WKNavigationDelegate
     var webView: WKWebView!
+    var progressView: UIProgressView!
     
     override func loadView() {
         webView = WKWebView() //
@@ -19,7 +20,18 @@ class ViewController: UIViewController, WKNavigationDelegate { //Nosso ViewCOntr
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
         
         let url = URL(string: "https://www.hackingwithswift.com")! //Swift guarda url num tipo especifico URL para aumentar as funcionalidades
         webView.load(URLRequest(url: url))//Coloca o url em um urlrequest porque diretamente não funciona
@@ -27,5 +39,23 @@ class ViewController: UIViewController, WKNavigationDelegate { //Nosso ViewCOntr
     }
 
 
+    @objc func openTapped(){
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem //importante para ipad, diz ao ios onde o actionsheet vai ficar ancorado
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction){
+        guard let actionTitle = action.title else { return }
+        guard let url = URL(string: "https://" + actionTitle) else { return }
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
 
